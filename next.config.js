916cf8dfa44a3acf378f6532e678aacc0f6acfa7
Elure <ioumih32@gmail.com>
2026-02-11 19:32:16 +0200

    Change X-Frame-Options to SAMEORIGIN

--- next.config.js
+++ next.config.js
@@ -70,7 +70,7 @@ const securityHeaders = [
     value: ContentSecurityPolicy.replace(/\n/g, ''),
   },
   { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
-  { key: 'X-Frame-Options', value: 'DENY' },
+  { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
   { key: 'X-Content-Type-Options', value: 'nosniff' },
   { key: 'X-DNS-Prefetch-Control', value: 'on' },
   { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains' },
